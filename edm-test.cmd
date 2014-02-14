5628f85ab752358c55f1f70dc9e347181324c619
Bruce Hill <bhill@slac.stanford.edu>
2014-02-14 04:50:53 +0000

    Updated chan controls to be more user friendly. Added new records to break out channel and event status bits into lots of individual bo records for easier monitoring
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/VHSx0x/current@15808 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm-test.cmd
+++ edm-test.cmd
@@ -1,29 +1,15 @@
 #! /bin/bash
 
 # Setup edm environment
-export EPICS_SITE_TOP=/reg/g/pcds/package/epics/3.14
-export EPICS_HOST_ARCH=$($EPICS_SITE_TOP/base/current/startup/EpicsHostArch.pl)
-
-export EDMFILES=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMFILTERS=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMHELPFILES=$EPICS_SITE_TOP/extensions/current/src/edm/helpFiles
-export EDMLIBS=$EPICS_SITE_TOP/extensions/current/lib/$EPICS_HOST_ARCH
-export EDMOBJECTS=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMPVOBJECTS=$EPICS_SITE_TOP/extensions/current/templates/edm
-export EDMUSERLIB=$EPICS_SITE_TOP/extensions/current/lib/$EPICS_HOST_ARCH
-export EDMACTIONS=/reg/g/pcds/package/tools/edm/config
-export EDMWEBBROWSER=mozilla
-export PATH=$PATH:$EPICS_SITE_TOP/extensions/current/bin/$EPICS_HOST_ARCH
-export EDMDATAFILES=".:.."
+source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
 edm -x -eolc		\
  -m "TITLE=High Voltage"		\
  -m "PARENT=EXP/highVoltage.edl"		\
  -m "IOC=AMO:R14:IOC:21"				\
- -m "DEV=AMO:R14:IOC:21:VHS9"			\
- -m "iVHS=9"							\
- -m "HOME=SXRHome.edl"					\
+ -m "DEV=AMO:R14:IOC:21:VHS0"			\
+ -m "iVHS=0"							\
+ -m "HOME=AMOHome.edl"					\
  vhsScreens/vhsN.edl					\
  vhsScreens/vhs2Chan.edl				\
- vhsScreens/VHSx0xTest.edl				\
  &

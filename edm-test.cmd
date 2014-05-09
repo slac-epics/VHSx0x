c1d0ce02e20ad918757d957252d5b8470f8c0e9a
Bruce Hill <bhill@slac.stanford.edu>
2014-05-09 04:21:29 +0000

    Updated iocScreens path
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/trunk/pcds/epics/modules/VHSx0x/current@16956 ececa60f-9f63-0410-97c7-ea05888eb260

--- edm-test.cmd
+++ edm-test.cmd
@@ -3,13 +3,17 @@
 # Setup edm environment
 source /reg/g/pcds/setup/epicsenv-3.14.12.sh
 
-edm -x -eolc		\
- -m "TITLE=High Voltage"		\
- -m "PARENT=EXP/highVoltage.edl"		\
- -m "IOC=AMO:R14:IOC:21"				\
- -m "DEV=AMO:R14:IOC:21:VHS0"			\
- -m "iVHS=0"							\
- -m "HOME=AMOHome.edl"					\
+# -m "PARENT=EXP/highVoltage.edl"		\
+# -m "IOC=AMO:R14:IOC:21"				\
+# -m "DEV=AMO:R14:IOC:21:VHS0"			\
+# -m "HOME=AMOHome.edl"					\
+
+edm -x -eolc							\
+ -m "TITLE=High Voltage"				\
+ -m "PARENT=gmd/gmdHome.edl"			\
+ -m "IOC=SXR:GMD:IOC:VME"				\
+ -m "DEV=SXR:GMD:SHV:VHS0"				\
+ -m "HOME=SXRHome.edl"					\
  vhsScreens/vhsN.edl					\
  vhsScreens/vhs2Chan.edl				\
  &
